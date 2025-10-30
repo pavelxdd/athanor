dad842e088c196323413186419fc1d70bef2d2d0
Pavel Otchertsov <pavel.otchertsov@gmail.com>
2025-10-30 19:24:13 +0500

    Remove reload menus

--- electron/main.ts
+++ electron/main.ts
@@ -265,8 +265,6 @@ async function buildMenu() {
       {
         label: 'View',
         submenu: [
-          { role: 'reload' as const },
-          { role: 'forceReload' as const },
           { role: 'toggleDevTools' as const },
           { type: 'separator' as const },
           { role: 'resetZoom' as const },
